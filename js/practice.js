cd3960c896594369fe64b39c12a1a2f2fc9225e8
jonathan-crepeau <jon.crepeau@gmail.com>
2022-08-01 12:23:43 -0700

    Constructor function and alter methods complete

--- js/practice.js
+++ js/practice.js
@@ -1,6 +1,18 @@
 // console.log('Fear is the mind killer.')
 
 class Person {
+    constructor(name, age, eyes, hair) {
+        this.legs = 2;
+        this.arms = 2;
+        this.name = name;
+        this.age = age;
+        this.eyes = eyes;
+        this.hair = hair;
+    }
+    // NOTE - You can use 'age' once again with this method, but probably it's best to use a different keyword then 'age' as it was used in the original constructor.
+    setAge(age) {
+        this.age = age;
+    }
     greet(otherPerson) {
         console.log('Hi ' + otherPerson + '!');
     }
@@ -9,5 +21,5 @@ class Person {
     }
 }
 
-const me = new Person();
+const me = new Person('Jonathan', 34, 'brown', 'brown');
 console.log(me);
