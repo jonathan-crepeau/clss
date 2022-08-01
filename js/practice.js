01da343da85322ed90b1e3805ead42b26552d267
jonathan-crepeau <jon.crepeau@gmail.com>
2022-08-01 12:20:03 -0700

    Class methods complete

---
+++ js/practice.js
@@ -0,0 +1,13 @@
+// console.log('Fear is the mind killer.')
+
+class Person {
+    greet(otherPerson) {
+        console.log('Hi ' + otherPerson + '!');
+    }
+    jump() {
+        console.log('Weeeee!');
+    }
+}
+
+const me = new Person();
+console.log(me);
