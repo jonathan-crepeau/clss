29fdecc39f490aed65a7a14dd017425b55471fcb
jonathan-crepeau <jon.crepeau@gmail.com>
2022-08-05 16:18:45 -0700

    'this' exercises, part 1 complete

--- js/protos.js
+++ js/protos.js
@@ -1,27 +1,20 @@
-// console.log('Hello there!')
+// let rabbit = {};
+// rabbit.speak = function(line) {
+//   console.log(`The rabbit says '${line}'`);
+// };
 
-// SECTION - One object, one function:
+// rabbit.speak("I'm alive.");
+// // → The rabbit says 'I'm alive.'
 
-const crepeauPrototype = {
-    greet() {
-        return 'Hello, my name is ' + this.firstName + ' Crepeau.';
-    }
+function speak(lineOfDialogue) {
+    console.log(`The ${this.type} rabbit says '${lineOfDialogue}'.`);
 }
 
-function Crepeau(firstName) {
-    this.firstName = firstName;
-}
+let whiteRabbit = { type: 'white', speak };
+let hungryRabbit = { type: 'hungry', speak };
 
-Object.setPrototypeOf(Crepeau.prototype, crepeauPrototype);
-// Object.assign(Crepeau.prototype, crepeauPrototype)
+// NOTE - You can think of 'this' as an extra parameter that is passed in a diffent way:
+whiteRabbit.speak('goody goody gum drops');
 
-const michele = new Crepeau('Michele');
-console.log(michele.greet());
-
-crepeauPrototype.jump = function() {return 'I am jumping!'};
-
-console.log(michele.jump())
-
-
-
-// SECTION - Two Functions
+// NOTE - To pass 'this' explicily, you can use a function's call() method, which takes the 'this' value as its first argument and treats futher arguments as normal parameters:
+speak.call(whiteRabbit, 'burp');
